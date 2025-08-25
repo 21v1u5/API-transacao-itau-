5c7f107c1edebe5d3324a8f51646d90e5f690df9
21v1u5 <4raujopenh4@gmail.com>
2025-08-25 14:20:13 -0300

    Comentando o código

--- src/main/java/com/itau/transacao_api/business/services/TransacaoService.java
+++ src/main/java/com/itau/transacao_api/business/services/TransacaoService.java
@@ -15,28 +15,38 @@ import java.util.List;
 @Slf4j
 public class TransacaoService {
 
+
+    // Armazenando na memória
     private final List<TransacaoRequestDTO> listaTransacoes = new ArrayList<>();
 
+
+    // Método para adicionar Transações
     public void adicionarTransacoes(TransacaoRequestDTO dto) {
 
         log.info("Iniciada o processamento de gravar transações");
 
+        // Primeira exceção
         if(dto.dataHora().isAfter(OffsetDateTime.now())){
-            log.error("Data e hora e hora maiores que a atual");
-            throw new UnprocessableEntity("Data e hora maiores que a data e hora atual");
+            log.error("Data e hora e hora maiores que a atual.");
+            throw new UnprocessableEntity("Data e hora maiores que a data e hora atuais.");
         }
+        // Segunda Exceção
         if(dto.valor() < 0){
-            log.error("Valor nao pode ser menor que zero");
-            throw new UnprocessableEntity("Valor nao pode ser menor que zero");
+            log.error("Valor não pode ser menor que zero.");
+            throw new UnprocessableEntity("Valor não pode ser menor que zero.");
         }
 
         listaTransacoes.add(dto);
     }
 
+
+    // Método para limpar Transações
     public void limparTransacoes(){
         listaTransacoes.clear();
     }
 
+
+    // Método para Buscar transações
     public List<TransacaoRequestDTO> buscarTransacoes(Integer intervaloBusca){
 
         OffsetDateTime dataHoraIntervalo = OffsetDateTime.now().minusSeconds(intervaloBusca);
